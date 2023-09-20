f6ff26b36da18f8077040b9d7d1bff700a12a174
Daniela Paz Lizama Aravena <dlizamaa.2brains@bancochile.cl>
2023-09-20 19:08:53 -0300

    cards maquetado

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,8 @@ module.exports = {
         'spin-low': 'spin 2s linear infinite', //linear, que se mantenga estable, infinite, que sea interminablemente
         'from-bellow': 'fromBellow 500ms linear', //linear, que se mantenga estable, infinite, que sea interminablemente
         'from-right':'fromRight 300ms linear',
+        'bg-banner': 'bgBanner 10s linear',
+        'text-banner': 'showBannerText 10s linear',
       },
       keyframes: {
         fromBellow:{
@@ -32,6 +34,34 @@ module.exports = {
           '0%': { transform: 'translateX(200%)'},
           '100%': { transform: 'translateX(0%)'}
         },
+        bgBanner: {
+          '0%': {'background-position': '0px 0px'},
+          '100%': { 'background-position' : '-200px -200px' },
+        },
+        showBannerText: {
+          '0%': {
+            transform: "translateX(-600%) scale(5,5)",
+            opacity: 1,
+
+          },
+          '50%': {
+            transform: "translateX(600%) scale(5,5)"
+          },
+          '75%': {
+            transform: "translateX(-600%) scale(5,5)",
+            'text-shadow': 'none',
+            color: 'transparent',
+            opacity: 0
+          },
+          '100%': {
+            transform: "translateX(0%) scale(1,1)",
+            'text-shadow': '2px 2px 1px black',
+            color: 'white',
+            opacity: 1
+            
+          },
+
+        }
       }
     },
     variants: {
